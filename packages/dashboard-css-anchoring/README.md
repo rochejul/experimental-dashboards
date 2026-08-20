9d9f69837fc715b4d26d8a4d40eaa4f26aefd792
Julien Roche <roche.jul@gmail.com>
2026-08-20 15:22:07 +0200

    feat(anchor,position): basic example

---
+++ packages/dashboard-css-anchoring/README.md
@@ -0,0 +1,19 @@
+# Dashboard - With anchor positioning API
+
+## Commands
+
+- `npm start`: Run a local web server (with https) to show the project
+
+## Resources
+
+- [https://master.dev/blog/drawing-a-line-to-connect-elements-with-css-anchor-positioning/](Drawing a Line to Connect Elements with CSS Anchor Positioning)
+- [https://rolandfranke.nl/frontend-stories/drawing-connections-with-css-anchor-positioning/](Drawing Connections with CSS Anchor Positioning)
+- [https://screencoordinates.com/javascript-get-mouse-coordinates/](JavaScript Get Mouse Coordinates)
+- [https://css-tricks.com/css-anchor-positioning-guide/](CSS Anchor Positioning Guide)
+- [https://developer.chrome.com/blog/tether-elements-to-each-other-with-css-anchor-positioning](Tether elements to each other with CSS anchor positioning)
+
+## Contributing
+
+- [Guidelines](../../docs/GUIDELINES.md)
+- [Contributing](../../docs/CONTRIBUTING.md)
+- [Code of conducts](../../docs/CODE_OF_CONDUCTS.md)
--- packages/dashboard-css-anchoring/app/app.js
+++ packages/dashboard-css-anchoring/app/app.js
@@ -1,1 +1,52 @@
-console.info('Hello world');
+function setupApp() {
+  let lastCursorPositionForBoxMovement = null;
+  let lastBoxPosition = null;
+
+  const bodyElement = document.querySelector('.app__body');
+  const box1Element = document.querySelector('.box--one');
+
+  bodyElement.addEventListener('pointermove', (event) => {
+    console.log('Pointer moved in %s %s', event.offsetX, event.offsetY);
+
+    if (lastBoxPosition) {
+      console.info('We compute the box position');
+      const diffX = Math.round(
+        event.clientX - lastCursorPositionForBoxMovement.x,
+      );
+      const diffY = Math.round(
+        event.clientY - lastCursorPositionForBoxMovement.y,
+      );
+
+      console.log('The box has moved from %s %s', diffX, diffY);
+
+      box1Element.style = `left: ${lastBoxPosition.x + diffX}px; top: ${lastBoxPosition.y + diffY}px;`;
+    }
+  });
+
+  box1Element.addEventListener('pointerdown', (event) => {
+    console.info(
+      'We will start to move the box from pointer position %s %s',
+      event.clientX,
+      event.clientY,
+    );
+
+    lastCursorPositionForBoxMovement = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+
+    const computedStyle = box1Element.computedStyleMap();
+    lastBoxPosition = {
+      x: computedStyle.get('left').value,
+      y: computedStyle.get('top').value,
+    };
+  });
+
+  box1Element.addEventListener('pointerup', (event) => {
+    console.info('We stop to move the box');
+    lastCursorPositionForBoxMovement = null;
+    lastBoxPosition = null;
+  });
+}
+
+setupApp();
